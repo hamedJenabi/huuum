3dff5a9a4adb007941a33f2959ba571979ce44be
Hamed <hamed.jenabi@gmail.com>
2022-02-23 22:42:30 +0100

    new design

--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Nastaliq+Urdu&family=Vibes&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Nastaliq+Urdu&family=Lato&family=Open+Sans:wght@300&display=swap"
           rel="stylesheet"
         />
       </Head>
-
+      {/* <div className={styles.bgWrapper}>
+        <img className={styles.bg} src="/hosna2.png" alt="" />
+      </div> */}
       <main className={styles.main}>
-        <h1 className={styles.title}>HUUUM </h1>
+        {/* <h1 className={styles.title}>HUUUM </h1>
         <h1 className={styles.title}>X.Y.Z. </h1>
         <h1 className={styles.persianTitle}>
           نه که خیلی خفنیم... گفتیم یه وبسایت درست کنیم
         </h1>
-        <h1 className={styles.title}>More info coming soon!</h1>
+        <h1 className={styles.title}>More info coming soon!</h1> */}
+        <div className={styles.titleWrapper}>
+          <p className={styles.title}>Bio</p>
+          <p className={styles.title}>Concerts</p>
+        </div>
       </main>
     </div>
   );
